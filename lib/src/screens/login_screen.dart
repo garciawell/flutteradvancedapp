ba3c9b981570b39dc9a9e5e00cad3d7e4252eb94
Wellington Garcia <garciawell@gmail.com>
2019-12-22 19:53:34 -0300

    Creating first field form

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'email@email.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {}
+
+  Widget submitButton() {}
+}
