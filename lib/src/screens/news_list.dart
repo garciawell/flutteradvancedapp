8c2e58ed591fab8151756afb9257ca967fbb976d
Wellington Garcia <garciawell@gmail.com>
2019-12-28 01:14:04 -0300

    Request api and loading data

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -5,7 +5,7 @@ import '../blocs/stories_provider.dart';
 class NewsList extends StatelessWidget {
   Widget build(context) {
     final bloc = StoriesProvider.of(context);
-
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
@@ -17,14 +17,16 @@ class NewsList extends StatelessWidget {
   Widget buildList(StoriesBloc bloc) {
     return StreamBuilder(
       stream: bloc.topIds,
-      builder: (context, snapshot) {
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still waiting on Ids');
+          return Center(
+            child: CircularProgressIndicator(),
+          );
         }
         return ListView.builder(
           itemCount: snapshot.data.length,
           itemBuilder: (contenxt, int index) {
-            return Text(snapshot.data[index]);
+            return Text('${snapshot.data[index]}');
           },
         );
       },
