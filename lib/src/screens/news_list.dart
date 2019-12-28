e097fd0cb2bbffedc3622a7daa5ad444245a4f6d
Wellington Garcia <garciawell@gmail.com>
2019-12-28 01:06:59 -0300

    Creating bloc news list with provider

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,38 +1,33 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:formflutter/src/blocs/stories_bloc.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
+      body: buildList(bloc),
     );
   }
 
-  Widget buildList() {
-    return ListView.builder(
-        itemCount: 1000,
-        itemBuilder: (context, int index) {
-          return FutureBuilder(
-            future: getFuture(),
-            builder: (context, snapshot) {
-              return Container(
-                height: 80.0,
-                child: snapshot.hasData
-                    ? Text('Im visible $index')
-                    : Text('I havent fetched data yey $index'),
-              );
-            },
-          );
-        });
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'Hi',
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Still waiting on Ids');
+        }
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (contenxt, int index) {
+            return Text(snapshot.data[index]);
+          },
+        );
+      },
     );
   }
 }
